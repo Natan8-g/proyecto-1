1b9976f4f36c3f6f2e8a7a563aa32e76f088d112
Natan Gamez <natangh90@outlook.com>
2024-07-11 11:59:39 -0600

    ejercicio mult

---
+++ src/gittt/Gittt.java
@@ -0,0 +1,18 @@
+
+package gittt;
+
+import java.util.Scanner;
+
+public class Gittt {
+
+    public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+        System.out.println("La tabla de multiplicar del 10 es: "); 
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(10 + "*" + i + "=" + 10 * i);
+        }
+        
+        
+    }
+    
+}
